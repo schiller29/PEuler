dd3485b3408716e49f048bdf733b57a1e3352642
schiller29 <mschiller@umass.edu>
2016-06-12 22:07:15 -0400

    solved the sum of 100! problem

---
+++ sumFactorial.py
@@ -0,0 +1,19 @@
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def sumNum(string):
+	sum = 0
+	x = len(string)
+	for y in range(0, x):
+		sum+= int(string[y])
+	return sum
+
+  
+bigString = str(factorial(100))
+print sumNum(bigString)
+ 
